9be40f49d293916da229a346dcf4b7d660dd9126
JoeVincentt <eugene.bb@hotmail.com>
2019-03-28 05:00:04 -0400

    finished

--- app.py
+++ app.py
@@ -8,15 +8,18 @@ elevation = list(data["ELEV"])
 name = list(data["NAME"])
 
 html = """<h4>Volcano information:</h4>
-Name: <a href="https://www.google.com/search?q={0}" target="_blank">{0}</a><br>
-
+Name: <a href="https://www.google.com/search?q={0}" target="_blank">{0}</a>
 <br/>
 Height: {1} m
 """
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=4, tiles='Mapbox Bright')
 
-fg = folium.FeatureGroup(name='My App')
+fgv = folium.FeatureGroup(name='Volcano Map')
+fgp = folium.FeatureGroup(name='Population')
+fgp.add_child(folium.GeoJson(
+    data=open("world.json", 'r', encoding="utf-8-sig").read(),
+    style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
 
 def marker_color(elevation):
@@ -31,11 +34,13 @@ def marker_color(elevation):
 
 for lat, lon, elevation, name in zip(lat, lon, elevation, name):
     elevation = str(elevation)
-
     iframe = folium.IFrame(html=html.format(
         name, elevation), width=200, height=100)
-    fg.add_child(folium.Marker(
+    fgv.add_child(folium.Marker(
         location=[lat, lon], popup=folium.Popup(iframe), icon=folium.Icon(color=marker_color(elevation))))
 
-map.add_child(fg)
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 map.save('Map1.html')
